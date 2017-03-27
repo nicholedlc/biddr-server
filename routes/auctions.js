23786b6fa4c5fe82d1ba32d5a5de3caa0fbe89ce
Nichole De La Cruz <nichole.dlc@gmail.com>
2017-03-27 11:27:09 -0700

    get auctions from database and display them in view template

--- routes/auctions.js
+++ routes/auctions.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const {Auction} = require('../models/index');
 
 router.get('/', function(req, res, next) {
-  res.render('auctions/index')
+  Auction
+    .findAll()
+    .then(auctions => {
+      res.render('auctions/index', {auctions})
+    })
+    .catch(res.send)
 });
 
 module.exports = router;
