63a36855ea3bd4f1977ad97a51a7a069dd7d0eee
Nichole De La Cruz <nichole.dlc@gmail.com>
2017-03-27 10:45:06 -0700

    create auctions router and partials

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
+const auctions = require('./routes/auctions');
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/auctions', auctions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
